d7c03b52adae040fe57bd93be811e329d84143a9
bunnyyrr <kate_kondrashova07@mail.ru>
2026-08-31 21:45:54 +0700

    feat: add JDBC connection manager

--- src/main/java/ru/bdayloop/Main.java
+++ src/main/java/ru/bdayloop/Main.java
@@ -1,4 +1,16 @@
 package ru.bdayloop;
 
+import ru.bdayloop.db.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main (String[] args){
+        try (Connection connection = ConnectionManager.getConnection()){
+            System.out.println("Подключились:" + !connection.isClosed());
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
