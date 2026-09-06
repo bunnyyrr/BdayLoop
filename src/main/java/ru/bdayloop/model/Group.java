2b875cd7762ffbd576492c1ecc8cf6f91b39bdb5
bunnyyrr <kate_kondrashova07@mail.ru>
2026-09-06 15:13:14 +0700

    feat: add Group entity

--- src/main/java/ru/bdayloop/model/Group.java
+++ src/main/java/ru/bdayloop/model/Group.java
@@ -1,4 +1,19 @@
 package ru.bdayloop.model;
 
 public class Group {
+    private int id;
+    private String name;
+
+    public Group(int id, String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
