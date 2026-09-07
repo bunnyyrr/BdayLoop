81648417c1a8793c90bdb767a2a5bd1e0e21eaf1
bunnyyrr <kate_kondrashova07@mail.ru>
2026-09-07 17:57:49 +0700

    feat: HTTP server (Jetty + servlet skeleton)

---
+++ src/main/java/ru/bdayloop/Application.java
@@ -0,0 +1,24 @@
+package ru.bdayloop;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import ru.bdayloop.web.HelloServlet;
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new HelloServlet()), "/hello");
+
+        server.start();
+        System.out.println("Сервер запущен на http://localhost:8080");
+        server.join();
+    }
+}
---
+++ src/main/java/ru/bdayloop/web/HelloServlet.java
@@ -0,0 +1,16 @@
+package ru.bdayloop.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Hello, world!");
+    }
+}
